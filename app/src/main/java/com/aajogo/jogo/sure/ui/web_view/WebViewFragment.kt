6555f02e5027ccd69092ce85b15af3034670f344
BABASTAS728 <Aberezkinn954@gmail.com>
2023-10-07 13:11:08 +0300

    WebView save state, layout changes

--- app/src/main/java/com/aajogo/jogo/sure/ui/web_view/WebViewFragment.kt
+++ app/src/main/java/com/aajogo/jogo/sure/ui/web_view/WebViewFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebChromeClient
+import android.webkit.WebSettings
 import android.webkit.WebViewClient
 import androidx.navigation.fragment.navArgs
 import com.aajogo.jogo.sure.databinding.FragmentWebViewBinding
@@ -17,6 +18,11 @@ class WebViewFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val args: WebViewFragmentArgs by navArgs()
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        binding.webView.saveState(outState)
+        super.onSaveInstanceState(outState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,7 +39,14 @@ class WebViewFragment : Fragment() {
             webView.webChromeClient = WebChromeClient()
             webView.settings.javaScriptEnabled = true
             webView.settings.domStorageEnabled = true
-            webView.loadUrl(args.url)
+            webView.settings.allowFileAccess = true
+            webView.settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+            webView.settings.javaScriptCanOpenWindowsAutomatically = true
+            if (savedInstanceState != null) {
+                webView.restoreState(savedInstanceState)
+            } else {
+                webView.loadUrl(args.url)
+            }
         }
     }
 
