0f0f0766cbaa17043c37444198f73206b492ecfa
BABASTAS728 <Aberezkinn954@gmail.com>
2023-10-08 14:58:30 +0300

    Load Photos feature

--- app/src/main/java/com/aajogo/jogo/sure/ui/web_view/WebViewFragment.kt
+++ app/src/main/java/com/aajogo/jogo/sure/ui/web_view/WebViewFragment.kt
@@ -1,13 +1,18 @@
 package com.aajogo.jogo.sure.ui.web_view
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
 import android.webkit.WebSettings
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.navigation.fragment.navArgs
 import com.aajogo.jogo.sure.databinding.FragmentWebViewBinding
@@ -18,30 +23,55 @@ class WebViewFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val args: WebViewFragmentArgs by navArgs()
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        binding.webView.saveState(outState)
-        super.onSaveInstanceState(outState)
-    }
+    var mFilePathCallback: ValueCallback<Array<Uri>>? = null
+    var mFileChooserParams: WebChromeClient.FileChooserParams? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         _binding = FragmentWebViewBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.webView.saveState(outState)
     }
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         with(binding) {
             webView.webViewClient = WebViewClient()
-            webView.webChromeClient = WebChromeClient()
+            webView.webChromeClient = object : WebChromeClient() {
+
+                override fun onShowFileChooser(
+                    webView: WebView?,
+                    filePathCallback: ValueCallback<Array<Uri>>?,
+                    fileChooserParams: FileChooserParams?,
+                ): Boolean {
+                    mFilePathCallback = filePathCallback
+                    mFileChooserParams = fileChooserParams
+
+                    val intent = Intent(Intent.ACTION_GET_CONTENT)
+                    intent.addCategory(Intent.CATEGORY_OPENABLE)
+                    intent.type = "*/*"
+                    val chooserIntent = Intent(Intent.ACTION_CHOOSER)
+                    chooserIntent.putExtra(Intent.EXTRA_INTENT, intent)
+                    chooserIntent.putExtra(Intent.EXTRA_TITLE, "Выберите действие")
+                    startActivityForResult(chooserIntent, FILE_CHOOSER_REQUEST_CODE)
+                    return true
+                }
+            }
             webView.settings.javaScriptEnabled = true
             webView.settings.domStorageEnabled = true
+            webView.settings.loadWithOverviewMode = true
+            webView.settings.useWideViewPort = true
             webView.settings.allowFileAccess = true
             webView.settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
             webView.settings.javaScriptCanOpenWindowsAutomatically = true
+            webView.settings.setSupportMultipleWindows(true)
+            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null)
             if (savedInstanceState != null) {
                 webView.restoreState(savedInstanceState)
             } else {
@@ -50,8 +80,27 @@ class WebViewFragment : Fragment() {
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == FILE_CHOOSER_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (data != null && data.data != null) {
+                    mFilePathCallback?.onReceiveValue(arrayOf(data.data!!))
+                }
+            } else {
+                mFilePathCallback?.onReceiveValue(null)
+            }
+
+            mFilePathCallback = null
+            mFileChooserParams = null
+        }
+    }
+
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
+    }
+
+    companion object {
+        private const val FILE_CHOOSER_REQUEST_CODE = 123
     }
 }
